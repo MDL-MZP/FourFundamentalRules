b8505ed512c2b2e6ecc43110753d3328b9c95846
Mazin-hub <1285543153@qq.com>
2020-04-10 11:59:46 +0800

    调整界面、更新功能

--- Team/src/Funtion/Accuracy.java
+++ Team/src/Funtion/Accuracy.java
@@ -9,19 +9,29 @@ import java.io.FileWriter;
  */
 public class Accuracy{
 
-   /**    输出正确率到文件中，需要比较userAnswers和Answers  */
+    /**
+     *  输出正确率到文件中，需要比较userAnswers和Answers
+     * @param userAnswers 用户答案
+     * @param grade 正确率文件
+     * @throws Exception
+     */
     public static void result(String[] userAnswers,File grade) throws Exception {
         //  拿到正确答案集，和用户答案做比较
+        // 正确题目数和错误题目数
         int correctNum = 0;
         int wrongNum = 0;
+        // 正确行输出
         String correctIndex = "";
+        // 错误行输出
         String wrongIndex = "";
+        // 得到标准答案数组
         String[] answers = CreateCorrectExpression.getAnswers();
         String[] write = new String[2];
         for (int i = 0; i < answers.length && answers[i] != null; i++) {
+            // 相同下标，用户答案和标准答案一样便是正确
             if (answers[i].equals(userAnswers[i])) {
                 correctNum++;
-                //  +1 是为了达到正确的题目序号
+                //  +1 是为了达到正确的题目序号，保存所有正确的题目序号
                 correctIndex += i + 1 + " ,";
             } else {
                 wrongNum++;
@@ -30,6 +40,7 @@ public class Accuracy{
         }
         StringBuffer s1 = new StringBuffer(correctIndex);
         StringBuffer s2 = new StringBuffer(wrongIndex);
+        // 正确数为0，就不加上后面的括号（）
         if (correctNum != 0){
             s1.insert(0, "( ");
             //  取代末尾的  , 变为 ）
@@ -41,12 +52,19 @@ public class Accuracy{
         }
         correctIndex = s1.toString();
         wrongIndex = s2.toString();
+        // 拼接
         write[0] = "Correct : " + correctNum + " " + correctIndex;
         write[1] = "Wrong : " + wrongNum + " " + wrongIndex;
+        // 写入文件
         write(grade,write);
     }
 
-    /**   专门用于Grade文件的写     */
+    /**
+     *  专门用于Grade文件的写
+     * @param fileName Grade.txt
+     * @param content 写入Grade.txt文件的内容
+     * @throws Exception
+     */
     private static void write(File fileName,String[] content) throws Exception{
         FileWriter fw = new FileWriter(fileName);
         for(int i = 0; i < content.length; i++) {
--- Team/src/Funtion/Input.java
+++ Team/src/Funtion/Input.java
@@ -4,123 +4,118 @@ import java.util.Scanner;
 
 /**
  * @author Mazin
- * switch case
+ * 输入
  */
 public class Input {
     public static int num;
-    public static int in;
+    public static int limits;
 
-    /**    输出基本信息    */
+    /**
+     *   输入基本信息，输出一些指导信息
+     */
     public static void inputBase(){
         Scanner sc = new Scanner(System.in);
         String[] s1;
         String str;
         print();
+        t:
+        // 表达式数目num没被赋值不结束
         while(num == 0) {
             System.out.println("\n请输入-n命令");
             str = sc.nextLine().trim();
             s1 = str.split(" +");
-
-
-            /**
-             * 加入这里输入-n -r 英文呢？？
-             */
+            // 要求必须“-n "，后面再加东西
             if ("-n".equals(s1[0]) && s1.length > 1) {
+                // for循环 要求必须输入数字
+                for (int j = 0; j < s1[1].length(); j++) {
+                    boolean tag = s1[1].charAt(j) >= 48 && s1[1].charAt(j) <= 57;
+                    if (!tag) {
+                        System.out.println("请勿输入不合规范的字符");
+                        continue t;
+                    }
+                }
+                if("0".equals(s1[1])){
+                    System.out.println("不符合要求的 0 题目数");
+                    continue;
+                }
                 num = Integer.parseInt(s1[1]);
                 break;
-            } else {
-                System.out.println("输入错误,请重新输入");
             }
+            System.out.println("输入错误,请重新输入");
         }
-        //说明一下in是干嘛的
-        while(in == 0) {
+       // 表达式数值范围limits不被赋值不结束
+        s:
+        while(limits == 0) {
             System.out.println("\n请输入-r命令");
             str = sc.nextLine().trim();
             s1 = str.split(" +");
             if ("-r".equals(s1[0]) && s1.length > 1) {
-                in = Integer.parseInt(s1[1]);
+                // for循环 要求必须输入数字
+                for (int j = 0; j < s1[1].length(); j++) {
+                    boolean tag = s1[1].charAt(j) >= 48 && s1[1].charAt(j) <= 57;
+                    if (!tag) {
+                        System.out.println("请勿输入不合规范的字符");
+                        continue s;
+                    }
+                }
+                if("0".equals(s1[1]) || "1".equals(s1[1])){
+                    System.out.println("不符合要求的 "+ s1[1] +" 数值范围");
+                    continue;
+                }
+                limits = Integer.parseInt(s1[1]);
                 break;
-            }else{
-                System.out.println("输入错误,请重新输入");
             }
+            System.out.println("输入错误,请重新输入");
         }
-        System.out.println("---------------------");
-        System.out.println("请打开Exercise.txt文件，并在这里答题");//操作说明详细一点
-        System.out.println("注：请在答案前注明序号并加“.”，否则不作答案录入，\n    且同一题号不同答案视为修改。答题顺序不做要求");
-        //给个例子
+        System.out.println("----------------------------------------------------");
+        System.out.println("【请在目录下的Exercise.txt文件答题！】");
+        System.out.println("真分数的输入请严格按照：“整数'分子/分母”形式输入！ ");
+        System.out.println("----------------------------------------------------\n");
     }
 
     private static void print(){
-        System.out.println("【自动生成四则运算器欢迎您！】");
-        System.out.println("请输入合格的命令行：");
-        System.out.println("-n x: 生成指定数目的题目x");
+        System.out.println("         👍❤ 自动生成四则运算器欢迎您！❤👍");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("请输入如以下合格的命令行：");
+        System.out.println("-n x: 生成指定数目的题目数x");
         System.out.println("-r x: 指定题目中数值的范围，[0,x)");
+        System.out.println("最后生成的练习、答案（Exercise.txt、Answer.txt）文件在当前目录下！");
     }
 
-    /**    扩展    */
+    /**
+     *  扩展输入内容
+     */
     public static boolean inputExpand(){
-        System.out.println("做完题后是否进行答案校对？");
-        System.out.println("正确命令：-e <exercisefile> -a <answerfile>，退出输入exit");
-        System.out.println("-e 指定你答题的题目文件，-a 指定比较的答案文件");
-        System.out.println("例如：-e Exercise.txt -a Answer.txt");
+        System.out.println("是否进行答案校对？ 修改完练习的答案后输入命令再次校对！！");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("|  ❤正确命令：-e <exercisefile> -a <answerfile>，退出输入exit❤   |");
+        System.out.println("|  -e 指定你答题的题目文件，-a 指定比较的答案文件                  |");
+        System.out.println("|  例如：-e Exercise.txt -a Answer.txt                             |");
+        System.out.println("-------------------------------------------------------------------");
         System.out.println("注：校对的结果和正确率在Grade.txt文件.");
         Scanner sc = new Scanner(System.in);
         String[] s;
         boolean tag;
         while(true) {
-            String st = sc.nextLine().trim();
+            String st = sc.nextLine();
             s = st.split(" ");
+            // 选择退出，不校对
             if("exit".equals(s[0])){
                 tag = false;
                 break;
             }
-            if (("-a".equals(s[0]) && "Answer.txt".equals(s[1]) && "-e".equals(s[2]) && "Exercise.txt".equals(s[3])) ||
-               ("-a".equals(s[2]) && "Answer.txt".equals(s[3]) && "-e".equals(s[0]) && "Exercise.txt".equals(s[1]))) {
-                tag = true;
-                break;
+            // 正确输入，校对
+            else if(s.length > 3) {
+                boolean flag = ("-a".equals(s[0]) && "Answer.txt".equals(s[1]) && "-e".equals(s[2]) && "Exercise.txt".equals(s[3])) ||
+                        ("-a".equals(s[2]) && "Answer.txt".equals(s[3]) && "-e".equals(s[0]) && "Exercise.txt".equals(s[1]));
+                if (flag) {
+                    tag = true;
+                    break;
+                }
             }
             System.out.println("输入有误，请重新输入");
         }
         return tag;
     }
 
-    /**     获取用户答案  */
-    /*
-    实现控制台输出题目，注意反馈错误信息，比如作答者乱输入东西
-     */
-    public static String[] usersAnswer(){
-//        String[] ques;
-//        ques = CreateCorrectExpression.getQuestions();
-        Scanner sc = new Scanner(System.in);
-        String[] s1 = new String[Input.num];
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < Input.num;){
-//            System.out.print(i + ques[i]);
-            String line = sc.nextLine();
-            if(sb.length() != 0) {
-                sb.delete(0, sb.length());
-            }
-            int j = 0;
-            //  题目序号
-            while(j < line.length() && line.charAt(j) != '.'){
-                sb.append(line.charAt(j++));
-            }
-            if(j == line.length()){
-                continue;
-            }
-            int index = Integer.parseInt(sb.toString());
-            sb.delete(0,sb.length());
-            //  序号的答案
-            while(++j < line.length()){
-                sb.append(line.charAt(j));
-            }
-            if(s1[index - 1] == null) {
-                s1[index - 1] = sb.toString().trim();
-                i++;
-            }else{
-                s1[index - 1] = sb.toString().trim();
-            }
-        }
-        return s1;
-    }
 }
